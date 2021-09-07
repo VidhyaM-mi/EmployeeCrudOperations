010c7e1a0b7f1ecc46b49e6a37b1f2a2f96d1a9a
Vidhya M <i-vidhya.m@monsterindia.com>
2021-09-07 11:01:34 +0530

    project lombok dependency

--- src/main/java/com/curdoperations/CrudOperations/Employee.java
+++ src/main/java/com/curdoperations/CrudOperations/Employee.java
@@ -1,42 +1,12 @@
 package com.curdoperations.CrudOperations;
+import lombok.*;
 
+@Builder
+@AllArgsConstructor
+@Data
 public class Employee {
-    String eid;
-    String name;
-    String email;
-    String contact;
-
-    public  Employee(){
-    }
-    public Employee(String eid, String name,String email,String contact) {
-        this.eid=eid;
-        this.name=name;
-        this.email=email;
-        this.contact=contact;
-    }
-    public String getEid() {
-        return eid;
-    }
-    public void setEid(String eid){
-        this.eid=eid;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email){
-        this.email=email;
-    }
-    public String getContact() {
-        return contact;
-    }
-    public void setContact(String contact){
-        this.contact=contact;
-    }
-
+   public  String eid;
+   private String name;
+   private String email;
+   private String contact;
 }
